a15baa60aa2319fcb5effdb4b954cc6e6ca447fb
Khaled FERJANI <kferjani@linagora.com>
2023-12-15 09:40:14 +0100

    cleanup

--- src/routes/animals/+page.server.ts
+++
@@ -1,8 +0,0 @@
-import prisma from '$lib/prisma';
-import type { PageServerLoad } from './$types';
-
-export const load: PageServerLoad = async () => {
-	const animals = await prisma.animal.findMany();
-
-	return { animals };
-};
