83be13496c383ec8cd1b00bbc481017b0b86cade
Khaled FERJANI <kferjani@linagora.com>
2024-01-10 14:35:28 +0100

    updated zod schemas

--- src/lib/schemas/index.ts
+++ src/lib/schemas/index.ts
@@ -41,4 +41,13 @@ export const updateInventoryItemSchema = addInventoryItemSchema.extend({
 
 export const deleteInventoryItemSchema = z.object({
 	id: z.string().min(1)
-})
+});
+
+export const addFundsSchema = z.object({
+	amount: z
+		.number()
+		.positive()
+		.or(z.string().regex(/\d+/).transform(Number))
+		.refine((n) => n > 0),
+	description: z.string().optional().default('')
+});
