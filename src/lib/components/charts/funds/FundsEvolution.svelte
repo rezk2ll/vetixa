dbc38aef9f3f8760083a6a4a19a76c9b4d9dd22a
Khaled FERJANI <kferjani@linagora.com>
2024-01-10 14:32:18 +0100

    added funds charts

---
+++ src/lib/components/charts/funds/FundsEvolution.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import Chart from 'chart.js/auto';
+
+	export let labels: string[];
+	export let data: number[];
+
+	onMount(async () => {
+		const ctx = (document.getElementById('funds-evolution-chart') as HTMLCanvasElement).getContext(
+			'2d'
+		);
+
+		if (ctx)
+			new Chart(ctx, {
+				type: 'line',
+				data: {
+					labels,
+					datasets: [
+						{
+							label: 'Évolution de la trésorerie',
+							data
+						}
+					]
+				},
+				options: {
+          responsive: true,
+					plugins: {
+						legend: {
+							position: 'top'
+						},
+						title: {
+							display: true,
+							text: ''
+						}
+					}
+				}
+			});
+	});
+</script>
+
+<div class="flex flex-col items-center justify-start w-full">
+	<div class="w-full px-5 pt-10 lg:py-5 bg-white shadow-2xl border-gray-200 xl:rounded">
+		<canvas id="funds-evolution-chart" />
+	</div>
+</div>
---
+++ src/lib/components/charts/funds/IncomeEvolution.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import Chart from 'chart.js/auto';
+
+	export let income: number[];
+	export let expense: number[];
+	export let labels: string[];
+
+	onMount(async () => {
+		const ctx = (document.getElementById('income-evolution-chart') as HTMLCanvasElement).getContext(
+			'2d'
+		);
+
+		if (ctx)
+			new Chart(ctx, {
+				type: 'line',
+				data: {
+					labels,
+					datasets: [
+						{
+							label: 'Revenus',
+							data: income,
+							borderColor: 'rgb(16, 185, 129)'
+						},
+						{
+							label: 'Dépenses',
+							data: expense,
+							borderColor: 'rgb(239, 68, 68)'
+						}
+					]
+				},
+				options: {
+          responsive: true,
+					plugins: {
+						legend: {
+							position: 'top'
+						},
+						title: {
+							display: true,
+							text: ''
+						}
+					}
+				}
+			});
+	});
+</script>
+
+<div class="flex flex-col items-center justify-start w-full">
+	<div class="w-full px-5 pt-10 lg:py-5 bg-white shadow-2xl border-gray-200 xl:rounded">
+		<canvas id="income-evolution-chart" />
+	</div>
+</div>
