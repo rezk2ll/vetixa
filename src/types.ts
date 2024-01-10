0aa6f32161383f47c282dfd0a1dbbe624fa22c9d
Khaled FERJANI <kferjani@linagora.com>
2024-01-10 14:34:33 +0100

    updated types

--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,9 @@
+import type { FundTransactionsResponse } from './pocketbase-types';
 
-export type StatusFilter = 'all' | 'available' | 'alert' | 'unavailable';
+export type storeStatusFilter = 'all' | 'available' | 'alert' | 'unavailable';
+export type fundsStatusFilter = 'all' | 'income' | 'expense';
+
+export interface Fund extends FundTransactionsResponse {
+	category: string;
+	description: string;
+}
