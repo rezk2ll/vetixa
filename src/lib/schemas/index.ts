2e43f3db02b267fd47ef6182ad2ea1432337e198
Khaled FERJANI <kferjani@linagora.com>
2024-01-12 10:04:36 +0100

    updated zod schemas

--- src/lib/schemas/index.ts
+++ src/lib/schemas/index.ts
@@ -55,3 +55,22 @@ export const addFundsSchema = z.object({
 export const updateQueueSchema = z.object({
 	id: z.string().min(1)
 });
+
+export const addAnimalSchema = z.object({
+	birthday: z.date().max(new Date()),
+	name: z.string().min(1),
+	sex: z.string().min(1),
+	type: z.string().min(1),
+	weight: z
+		.number()
+		.or(z.string().regex(/\d+/).transform(Number))
+		.refine((n) => n > 0)
+});
+
+export const updateAnimalSchema = addAnimalSchema.extend({
+	id: z.string().min(1)
+});
+
+export const removeAnimalSchema = z.object({
+	id: z.string().min(1)
+});
