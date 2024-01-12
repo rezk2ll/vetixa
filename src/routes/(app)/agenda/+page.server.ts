022ff617e0f3d4804c3307be57b8062d51618e30
Khaled FERJANI <kferjani@linagora.com>
2024-01-12 17:01:44 +0100

    added agenda server page

---
+++ src/routes/(app)/agenda/+page.server.ts
@@ -0,0 +1,70 @@
+import type { AgendaResponse } from '$root/pocketbase-types';
+import { message, superValidate } from 'sveltekit-superforms/server';
+import type { Actions, PageServerLoad } from './$types';
+import { addAgendaEventSchema, removeSchema, updateAgendaEventSchema } from '$root/lib/schemas';
+
+export const load = (async ({ locals: { pb } }) => {
+	const addForm = await superValidate(addAgendaEventSchema, { id: 'add-event' });
+	const updateForm = await superValidate(updateAgendaEventSchema, { id: 'update-event' });
+	const removeForm = await superValidate(removeSchema, { id: 'remove-event' });
+
+	const events = await pb.collection('agenda').getFullList<AgendaResponse>();
+
+	return { events, addForm, updateForm, removeForm };
+}) satisfies PageServerLoad;
+
+export const actions: Actions = {
+	addEvent: async ({ request, locals: { pb } }) => {
+		const form = await superValidate(request, addAgendaEventSchema);
+
+		try {
+			if (!form.valid) {
+				return message(form, 'invalid data', { status: 400 });
+			}
+			
+			await pb.collection('agenda').create(form.data);
+		} catch (error) {
+			console.error(error);
+
+			return message(form, 'something went wrong', { status: 500 });
+		}
+
+		return { form };
+	},
+
+	updateEvent: async ({ request, locals: { pb } }) => {
+		const form = await superValidate(request, updateAgendaEventSchema);
+
+		try {
+			if (!form.valid) {
+				return message(form, 'invalid data', { status: 400 });
+			}
+
+			await pb.collection('agenda').update(form.data.id, form.data);
+		} catch (error) {
+			console.error(error);
+
+			return message(form, 'something went wrong', { status: 500 });
+		}
+
+		return { form };
+	},
+
+	removeEvent: async ({ request, locals: { pb } }) => {
+		const form = await superValidate(request, removeSchema);
+
+		try {
+			if (!form.valid) {
+				return message(form, 'invalid data', { status: 400 });
+			}
+
+			await pb.collection('agenda').delete(form.data.id);
+		} catch (error) {
+			console.error(error);
+
+			return message(form, 'something went wrong', { status: 500 });
+		}
+
+		return { form };
+	}
+};
--- src/routes/(app)/agenda/+page.svelte
+++ src/routes/(app)/agenda/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import type { AgendaResponse } from '$root/pocketbase-types';
 	import { Calendar } from '@fullcalendar/core';
 	import dayGridPlugin from '@fullcalendar/daygrid';
 	import interactionPlugin from '@fullcalendar/interaction';
@@ -6,57 +7,32 @@
 	import listPlugin from '@fullcalendar/list';
 	import { onMount } from 'svelte';
 	import french from '@fullcalendar/core/locales/fr';
+	import type { PageData } from './$types';
+	import Modal from '$root/lib/components/Modal.svelte';
+	import AddEventForm from '$root/lib/components/forms/agenda/AddEventForm.svelte';
+
+	export let data: PageData;
+	$: ({ events } = data);
 
 	let calendarRef: HTMLElement;
+	let openAddModal = false;
+	let start: Date;
+	let end: Date;
+	let calendar: Calendar;
 
-	const events = [
-		{
-			title: 'Operation',
-			start: '2023-11-15'
-		},
-		{
-			groupId: '999',
-			title: 'Reunion equipe',
-			start: '2023-11-14T16:00:00'
-		},
-		{
-			groupId: '999',
-			title: 'Reunion rania',
-			start: '2023-11-14T16:30:00'
-		},
-		{
-			title: 'Operation chien',
-			start: '2023-11-14',
-			end: '2023-11-14'
-		},
-		{
-			title: 'Operation chien',
-			start: '2023-11-16T09:00:00',
-			end: '2023-11-16T10:00:00'
-		},
-		{
-			title: 'Operation chat Jack',
-			start: '2023-11-16T16:00:00',
-			end: '2023-11-16T17:00:00'
-		},
-		{
-			title: 'RDV Jihen Hammemi',
-			start: '2023-11-14T10:30:00',
-			end: '2023-11-14T12:30:00'
-		},
-		{
-			title: 'RDV Khaled',
-			start: '2023-11-14T12:00:00'
-		},
-		{
-			title: 'RDV Dhouha',
-			start: '2023-11-14T14:30:00',
-			end: '2023-11-14T15:30:00'
-		}
-	];
+	$: getEvents = (
+		_fetchInfo: any,
+		successCallback: (arg0: AgendaResponse[]) => void,
+		_failureCallback: any
+	) => {
+		successCallback(events);
+	};
 
 	onMount(() => {
-		let calendar = new Calendar(calendarRef, {
+		calendar = new Calendar(calendarRef, {
+			initialView: 'timeGridWeek',
+			scrollTime: '08:00:00',
+			selectable: true,
 			plugins: [interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin],
 			locale: french,
 			headerToolbar: {
@@ -67,12 +43,26 @@
 			height: 'auto',
 			navLinks: true,
 			editable: true,
-			events
+			eventSources: [getEvents],
+			select: (info) => {
+				openAddModal = true;
+
+				start = info.start;
+				end = info.end;
+			}
 		});
 
 		calendar.render();
 	});
+
+	$: events && calendar && calendar.refetchEvents();
 </script>
+
+<Modal bind:open={openAddModal} size="medium">
+	{#if start && end}
+		<AddEventForm bind:open={openAddModal} {start} {end} />
+	{/if}
+</Modal>
 
 <div
 	class="w-full flex items-center xl:pt-10 h-auto justify-center overscroll-none overflow-hidden xl:pb-10"
@@ -89,6 +79,6 @@
 
 <style lang="postcss">
 	:global(.fc-toolbar-title) {
-    @apply text-sm
+		@apply text-sm;
 	}
 </style>
