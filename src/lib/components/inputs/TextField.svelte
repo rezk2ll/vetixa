94878c175dcde3da89ab8177b62f3c33f023a3c9
Khaled FERJANI <kferjani@linagora.com>
2024-01-12 11:54:21 +0100

    updated text field to support optional fields

--- src/lib/components/inputs/TextField.svelte
+++ src/lib/components/inputs/TextField.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	export let label: string;
 	export let placeholder: string = "";
-	export let value: string;
+	export let value: string | undefined;
 	export let isInValid: boolean;
 	export let name: string;
 	export let required: boolean = true;
