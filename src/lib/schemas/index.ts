54c9194a71c86d219ed9c70c802ae5f6e5aa4647
Khaled FERJANI <kferjani@linagora.com>
2024-01-12 11:54:52 +0100

    updated zod schemas: added client

--- src/lib/schemas/index.ts
+++ src/lib/schemas/index.ts
@@ -74,3 +74,29 @@ export const updateAnimalSchema = addAnimalSchema.extend({
 export const removeAnimalSchema = z.object({
 	id: z.string().min(1)
 });
+
+export const clientSchema = z.object({
+	firstname: z.string().min(1),
+	lastname: z.string().min(1),
+	email: z.string().email().optional(),
+	tel: z.string().min(1),
+	address: z.string().optional()
+});
+
+export const addClientSchema = clientSchema.transform((client) => ({
+	...client,
+	name: `${client.firstname} ${client.lastname}`
+}));
+
+export const updateClientSchema = clientSchema
+	.extend({
+		id: z.string().min(1)
+	})
+	.transform((client) => ({
+		...client,
+		name: `${client.firstname} ${client.lastname}`
+	}));
+
+export const removeClientSchema = z.object({
+	id: z.string().min(1)
+});
