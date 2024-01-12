ed967a19148a12094bf47b3004d199c38f6952d9
Khaled FERJANI <kferjani@linagora.com>
2024-01-12 11:53:30 +0100

    added client forms

---
+++ src/lib/components/forms/clients/AddClientForm.svelte
@@ -0,0 +1,84 @@
+<script lang="ts">
+	import TextField from '$lib/components/inputs/TextField.svelte';
+	import { addClientFormStore } from '$lib/store/clients';
+	import { superForm } from 'sveltekit-superforms/client';
+
+	export let open = false;
+
+	const { form, enhance } = superForm($addClientFormStore, {
+		onResult: ({ result }) => {
+			if (result.type === 'success') {
+				open = false;
+			}
+		},
+		taintedMessage: null
+	});
+</script>
+
+<div>
+	<div class="flex items-center justify-center">
+		<svg
+			fill="none"
+			class="w-8 h-8 text-gray-700 dark:text-gray-300"
+			stroke="currentColor"
+			stroke-width="1.5"
+			viewBox="0 0 24 24"
+			xmlns="http://www.w3.org/2000/svg"
+			aria-hidden="true"
+		>
+			<path
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+			/>
+		</svg>
+	</div>
+
+	<div class="mt-2 text-center">
+		<h3
+			class="text-lg font-medium leading-6 text-gray-800 capitalize dark:text-white"
+			id="modal-title"
+		>
+			Nouveau client
+		</h3>
+		<p class="mt-2 text-sm text-gray-500 dark:text-gray-400">
+			Veuillez remplir le formulaire ci-dessous avec des détails précis
+		</p>
+	</div>
+</div>
+
+<form use:enhance action="/clients/?/addClient" class="mt-4" method="POST">
+	<TextField name="lastname" label="Nom" bind:value={$form.lastname} isInValid={false} />
+	<TextField name="firstname" label="prénom" bind:value={$form.firstname} isInValid={false} />
+	<TextField name="tel" label="Numéro de téléphone" bind:value={$form.tel} isInValid={false} />
+	<TextField
+		name="email"
+		label="Adresse Email"
+		bind:value={$form.email}
+		isInValid={false}
+		required={false}
+	/>
+	<TextField
+		name="address"
+		label="Adresse"
+		bind:value={$form.address}
+		isInValid={false}
+		required={false}
+	/>
+	<div class="mt-4 sm:flex sm:items-center sm:-mx-2">
+		<button
+			type="button"
+			on:click={() => (open = false)}
+			class="w-full px-4 py-2 text-sm font-medium tracking-wide text-gray-700 capitalize transition-colors duration-300 transform border border-gray-200 rounded-md sm:w-1/2 sm:mx-2 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800 hover:bg-gray-100 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-40"
+		>
+			Annuler
+		</button>
+
+		<button
+			type="submit"
+			class="w-full px-4 py-2 mt-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-blue-600 rounded-md sm:mt-0 sm:w-1/2 sm:mx-2 hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-40"
+		>
+			Confirmer
+		</button>
+	</div>
+</form>
