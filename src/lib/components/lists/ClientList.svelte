fd5e6c3b3a9b1e75fde52b72e98b872cf7929bdd
Khaled FERJANI <kferjani@linagora.com>
2024-01-12 11:54:30 +0100

    updated clients list

--- src/lib/components/lists/ClientList.svelte
+++ src/lib/components/lists/ClientList.svelte
@@ -1,35 +1,83 @@
 <script lang="ts">
-	import AddClientForm from '$lib/components/forms/AddClientForm.svelte';
-	import Avatar from '$lib/components/Avatar.svelte';
 	import Modal from '$lib/components/Modal.svelte';
+	import type { IClient } from '$lib/../types';
 	import ConfirmationDialog from '../ConfirmationDialog.svelte';
-	import type { ClientsResponse } from '../../../pocketbase-types';
+	import { superForm } from 'sveltekit-superforms/client';
+	import AddAnimalForm from '../forms/animals/AddAnimalForm.svelte';
+	import UpdateAnimalForm from '../forms/animals/updateAnimalForm.svelte';
+	import { clients, removeClientFormStore } from '$lib/store/clients';
+	import Avatar from '$lib/components/Avatar.svelte';
+	import AddClientForm from '../forms/clients/AddClientForm.svelte';
+	import UpdateClientForm from '../forms/clients/UpdateClientForm.svelte';
 
-	let openAddClientModal = false;
-	let deleteFormRef: HTMLFormElement;
-	let selectedItem: ClientsResponse | null;
+	let openAddModal = false;
+	let openUpdateModal = false;
+	let search: string;
+	let page = 0;
 	let showConfirmation = false;
+	let selectedItem: IClient | null;
+	let deleteFormRef: HTMLFormElement;
+	let selectedUpdateItem: IClient | null;
 
-	$: handler = () => {
+	const totalPages = Math.ceil($clients.length / 10);
+
+	$: items = $clients.filter((item) => {
+		if (search && search.length) {
+			return (
+				item.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()) ||
+				item.tel.toLocaleLowerCase().includes(search.toLocaleLowerCase()) ||
+				item.email.toLocaleLowerCase().includes(search.toLocaleLowerCase()) ||
+				item.animals.find((animal) =>
+					animal.name.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+				)
+			);
+		}
+
+		return true;
+	});
+
+	$: pageItems = items.slice(page * 10, page * 10 + 10);
+
+	const handler = () => {
 		deleteFormRef.requestSubmit();
 
 		selectedItem = null;
 		showConfirmation = false;
 	};
 
-	const remove = (item: ClientsResponse) => {
+	const remove = (item: IClient) => {
+		$deleteForm.id = item.id;
 		selectedItem = item;
 		showConfirmation = true;
 	};
 
-	export let data: ({
-		animals: { sex: string; name: string }[];
-	} & ClientsResponse)[];
+	const update = (item: IClient) => {
+		selectedUpdateItem = item;
+		openUpdateModal = true;
+	};
+
+	const { enhance, form: deleteForm } = superForm($removeClientFormStore, {
+		onResult: ({ result }) => {
+			if (result.type === 'success') {
+				showConfirmation = false;
+			}
+		},
+		dataType: 'json'
+	});
 </script>
 
-<form action="?/delete" method="POST" class="hidden" bind:this={deleteFormRef}>
+<Modal bind:open={openAddModal} size="medium">
+	<AddClientForm bind:open={openAddModal} />
+</Modal>
+<Modal bind:open={openUpdateModal} size="medium">
+	{#if selectedUpdateItem}
+		<UpdateClientForm bind:open={openUpdateModal} bind:item={selectedUpdateItem} />
+	{/if}
+</Modal>
+
+<form use:enhance action="?/removeClient" method="POST" class="hidden" bind:this={deleteFormRef}>
 	{#if selectedItem}
-		<input type="hidden" name="id" bind:value={selectedItem.id} />
+		<input type="hidden" name="id" bind:value={$deleteForm.id} />
 	{/if}
 </form>
 
@@ -40,54 +88,85 @@
 				Supprimer {selectedItem?.name}
 			</h3>
 			<p class="mt-2 text-sm text-gray-500 dark:text-gray-400">
-				Êtes-vous sûr de vouloir supprimer ce client ? Toutes vos données seront définitivement
+				Êtes-vous sûr de vouloir supprimer cet animal ? Toutes vos données seront définitivement
 				supprimé. Cette action ne peut pas être annulée.
 			</p>
 		</div>
 	</div>
 </ConfirmationDialog>
-<Modal bind:open={openAddClientModal}>
-	<AddClientForm bind:open={openAddClientModal} />
-</Modal>
-<div class="antialiased" data-sveltekit-preload-data="hover">
-	<div class="flex flex-col items-center justify-start lg:pt-20 xl:pt-10 xl:pl-14">
-		<div
-			class="w-full xl:w-11/12 px-1 pt-10 lg:p-10 bg-white shadow-2xl border-gray-200 h-screen xl:h-fit xl:rounded-2xl"
-		>
-			<div class="flex items-center gap-x-3 w-full">
-				<div class="w-full grow flex items-center justify-center gap-x-3 xl:px-1 xl:justify-start">
-					<h2 class="text-lg font-medium text-gray-800 dark:text-white">Clients</h2>
 
-					<span class="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full">{data.length}</span
-					>
+<div class="flex flex-col items-center justify-start xl:pl-14 w-full">
+	<div class="w-full px-5 pt-10 lg:p-5 bg-white shadow-2xl border-gray-200 xl:rounded">
+		<div class="flex flex-col space-y-4">
+			<div class="flex flex-col lg:flex-row items-start lg:items-center justify-between w-full gr">
+				<div>
+					<div class="flex items-center gap-x-3">
+						<h2 class="text-lg font-medium text-gray-800 dark:text-white">Clients</h2>
+						<span
+							class="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full dark:bg-gray-800 dark:text-blue-400"
+							>{$clients.length}</span
+						>
+					</div>
+
+					<p class="mt-1 text-sm text-gray-500 dark:text-gray-300">La liste des clients</p>
 				</div>
-				<button
-					class="flex-shrink hover:bg-gray-300 p-2 rounded-full flex space-x-5 flex-row"
-					on:click={() => (openAddClientModal = true)}
-				>
-					Ajouter
-					<svg
-						class="w-6"
-						fill="none"
-						stroke="currentColor"
-						stroke-width="1.5"
-						viewBox="0 0 24 24"
-						xmlns="http://www.w3.org/2000/svg"
-						aria-hidden="true"
+				<div class="flex items-center mt-4 gap-x-2">
+					<button
+						on:click={() => (openAddModal = true)}
+						class="flex items-center justify-center w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-emerald-500 rounded-lg shrink-0 sm:w-auto gap-x-2 hover:bg-emerald-600"
 					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z"
-						/>
-					</svg>
-				</button>
-			</div>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="w-5 h-5"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+							/>
+						</svg>
 
+						<span>Nouveau client</span>
+					</button>
+				</div>
+			</div>
+			<div
+				class="flex flex-col lg:flex-row items-start gap-4 lg:gap-0 lg:items-center justify-between"
+			>
+				<div class="flex items-center mt-0 h-6 w-full">
+					<span class="absolute">
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="w-5 h-5 mx-3 text-gray-400 dark:text-gray-600"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+							/>
+						</svg>
+					</span>
+
+					<input
+						bind:value={search}
+						type="text"
+						placeholder="Chercher par nom, email, téléphone, animal..."
+						class="block w-full py-1.5 pr-5 text-gray-700 bg-white border border-gray-200 rounded-lg md:w-4/12 placeholder-gray-400/70 pl-11 rtl:pr-11 rtl:pl-5 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+					/>
+				</div>
+			</div>
 			<div class="flex flex-col mt-6">
 				<div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-					<div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-						<div class="overflow-hidden dark:border-gray-700 md:rounded-lg">
+					<div class="min-w-full py-2 align-middle md:px-6 lg:px-8">
+						<div class="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
 							<table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
 								<thead class="bg-gray-50 dark:bg-gray-800">
 									<tr>
@@ -130,15 +209,15 @@
 								<tbody
 									class="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900"
 								>
-									{#each data as client}
+									{#each pageItems as item}
 										<tr>
 											<td class="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
 												<div class="inline-flex items-center gap-x-3">
-													<a href="/clients/{client.id}" class="flex items-center gap-x-2">
-														<Avatar name={client.name} />
+													<a href="/clients/{item.id}" class="flex items-center gap-x-2">
+														<Avatar name={item.name} />
 														<div>
 															<h2 class="font-medium text-gray-800 dark:text-white hover:underline">
-																{client.name}
+																{item.name}
 															</h2>
 														</div>
 													</a>
@@ -146,19 +225,19 @@
 											</td>
 											<td
 												class="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap"
-												>{client.email ?? '-'}</td
+												>{item.email ?? '-'}</td
 											>
 											<td
 												class="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap"
-												>{client.tel ?? '-'}</td
+												>{item.tel ?? '-'}</td
 											>
 											<td
 												class="px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap"
-												>{client.address ?? '-'}</td
+												>{item.address ?? '-'}</td
 											>
 											<td class="px-4 py-4 text-sm whitespace-nowrap">
 												<div class="flex items-center gap-x-2">
-													{#each client.animals as animal}
+													{#each item.animals as animal}
 														<p
 															class="px-3 py-1 text-xs {animal.sex === 'male'
 																? 'text-blue-500 bg-blue-100/60'
@@ -172,7 +251,7 @@
 											<td class="px-4 py-4 text-sm whitespace-nowrap">
 												<div class="flex items-end justify-end gap-x-6 w-full">
 													<button
-														on:click={() => remove(client)}
+														on:click={() => remove(item)}
 														class="text-gray-500 transition-colors duration-200 dark:hover:text-red-500 dark:text-gray-300 hover:text-red-500 focus:outline-none"
 													>
 														<svg
@@ -191,8 +270,8 @@
 														</svg>
 													</button>
 
-													<a
-														href="/clients/{client.id}"
+													<button
+														on:click={() => update(item)}
 														title="Modifier le client"
 														class="text-gray-500 transition-colors duration-200 dark:hover:text-yellow-500 dark:text-gray-300 hover:text-yellow-500 focus:outline-none"
 													>
@@ -210,7 +289,7 @@
 																d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
 															/>
 														</svg>
-													</a>
+													</button>
 												</div>
 											</td>
 										</tr>
@@ -222,57 +301,70 @@
 				</div>
 			</div>
 
-			<div class="flex items-center justify-between mt-6">
-				<a
-					href="#"
-					class="flex items-center px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800"
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="w-5 h-5 rtl:-scale-x-100"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
-						/>
-					</svg>
-
-					<span> précédent </span>
-				</a>
-
-				<div class="items-center hidden lg:flex gap-x-3">
-					<a
-						href="#"
-						class="px-2 py-1 text-sm text-blue-500 rounded-md dark:bg-gray-800 bg-blue-100/60">1</a
+			<div class="mt-6 sm:flex sm:items-center sm:justify-between">
+				<div class="text-sm text-gray-500 dark:text-gray-400">
+					Page <span class="font-medium text-gray-700 dark:text-gray-100"
+						>{page + 1} sur {totalPages}</span
 					>
 				</div>
 
-				<a
-					href="#"
-					class="flex items-center px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800"
-				>
-					<span> Suivant </span>
-
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke-width="1.5"
-						stroke="currentColor"
-						class="w-5 h-5 rtl:-scale-x-100"
+				<div class="flex items-center mt-4 gap-x-4 sm:mt-0">
+					<button
+						on:click={() => (page -= 1)}
+						disabled={page <= 0}
+						class="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 {page <=
+						0
+							? 'bg-slate-200'
+							: 'bg-white'} border rounded-md sm:w-auto gap-x-2 {page <= 0
+							? 'hover:bg-slate-200'
+							: 'hover:bg-gray-100'}  dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800"
 					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-						/>
-					</svg>
-				</a>
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="w-5 h-5 rtl:-scale-x-100"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
+							/>
+						</svg>
+
+						<span> précédent </span>
+					</button>
+
+					<button
+						disabled={page >= totalPages - 1}
+						on:click={() => (page += 1)}
+						class="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 {page >=
+						totalPages - 1
+							? 'bg-slate-200'
+							: 'bg-white'} border rounded-md sm:w-auto gap-x-2 {page >= totalPages - 1
+							? 'hover:bg-slate-200'
+							: 'hover:bg-gray-100'} dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800"
+					>
+						<span> Suivant </span>
+
+						<svg
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							class="w-5 h-5 rtl:-scale-x-100"
+						>
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+							/>
+						</svg>
+					</button>
+				</div>
 			</div>
 		</div>
 	</div>
