32521c6f495755be6a8794d5368fcc37527c2728
Khaled FERJANI <kferjani@linagora.com>
2024-01-15 11:24:06 +0100

    updated confirmation dialog z-index

--- src/lib/components/ConfirmationDialog.svelte
+++ src/lib/components/ConfirmationDialog.svelte
@@ -5,14 +5,14 @@
 </script>
 
 <div
-	class="relative z-10 {show ? '' : 'hidden'}"
+	class="relative z-50 {show ? '' : 'hidden'}"
 	aria-labelledby="modal-title"
 	role="dialog"
 	aria-modal="true"
 >
 	<div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
 
-	<div class="fixed inset-0 z-10 w-screen overflow-y-auto">
+	<div class="fixed inset-0 z-50 w-screen overflow-y-auto">
 		<div class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
 			<div
 				class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg"
