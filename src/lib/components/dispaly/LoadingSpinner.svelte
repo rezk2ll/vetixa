d5e5eaf19c4486f247fe27a64608e1fda5c5f92f
Khaled FERJANI <kferjani@linagora.com>
2024-01-22 15:21:59 +0100

    added spinner

---
+++ src/lib/components/dispaly/LoadingSpinner.svelte
@@ -0,0 +1,6 @@
+<div
+	class="absolute right-1/2 bottom-1/2 transform translate-x-1/2 translate-y-1/2 flex justify-center items-center"
+>
+	<div class="absolute animate-spin rounded-full h-40 w-40 border-t-4 border-b-4 border-blue-400" />
+	<img src="/dog.png" class="rounded-full h-36 w-36" alt="loading" />
+</div>
