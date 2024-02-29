8c415784c7c16a25a9d6aefd0a065e55f58d7f87
Khaled FERJANI <kferjani@linagora.com>
2024-02-29 17:18:38 +0100

    💄 Chore: use flex layout in paiement method

--- src/lib/components/forms/inventory/SellInventoryItemForm.svelte
+++ src/lib/components/forms/inventory/SellInventoryItemForm.svelte
@@ -5,8 +5,11 @@
 	import Select from 'svelte-select';
 	import NumberField from '$lib/components/inputs/NumberField.svelte';
 	import SubmitButton from '$lib/components/buttons/SubmitButton.svelte';
+	import type { PaymentMethodType } from '$root/types';
 
 	export let open = false;
+
+	const methods: PaymentMethodType[] = ['cash', 'tpe', 'cheque'];
 
 	const { form, enhance, submitting } = superForm($sellInventoryFormStore, {
 		clearOnSubmit: 'errors-and-message',
@@ -88,11 +91,19 @@
 
 		return acc;
 	}, {} as Record<string, any>);
+
 	$: total = $form.items.reduce((acc, curr) => {
 		const record = itemRecords[curr.id];
 
 		return acc + record.price * curr.quantity;
 	}, 0);
+
+	const handleMethodChange = (e: any) => {
+		if (e.detail.value !== 'cash') {
+			$form.incash = 0;
+			$form.outcash = 0;
+		}
+	};
 </script>
 
 <div>
@@ -139,28 +150,59 @@
 </div>
 <hr />
 
-<div class="relative bottom-0 flex p-3 bg-white dark:bg-gray-900">
-	<div class="object-cover object-center w-10 h-10 rounded-full">
-		<svg
-			data-slot="icon"
-			fill="none"
-			stroke-width="1.5"
-			stroke="currentColor"
-			viewBox="0 0 24 24"
-			xmlns="http://www.w3.org/2000/svg"
-			aria-hidden="true"
-		>
-			<path
-				stroke-linecap="round"
-				stroke-linejoin="round"
-				d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z"
-			/>
-		</svg>
+<div class="relative bottom-0 flex p-3 bg-white dark:bg-gray-900 w-full">
+	<div class="flex items-center justify-center grow">
+		<div class="object-cover object-center w-10 h-10 rounded-full">
+			<svg
+				data-slot="icon"
+				fill="none"
+				stroke-width="1.5"
+				stroke="currentColor"
+				viewBox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg"
+				aria-hidden="true"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z"
+				/>
+			</svg>
+		</div>
+
+		<div class="mx-4">
+			<h1 class="text-sm text-gray-700 dark:text-gray-200 font-semibold">Total</h1>
+			<p class="text-sm text-gray-500 dark:text-gray-400 font-semibold">{total} dt</p>
+		</div>
 	</div>
 
-	<div class="mx-4">
-		<h1 class="text-sm text-gray-700 dark:text-gray-200">Total</h1>
-		<p class="text-sm text-gray-500 dark:text-gray-400">{total} dt</p>
+	<div class="w-full lg:w-1/2 px-10">
+		<Select
+			items={methods}
+			disabled={total < 1}
+			listOffset={10}
+			bind:value={$form.method}
+			on:change={handleMethodChange}
+		/>
+		<!-- TODO: fix value -->
+		{#if $form.method !== 'cash'}
+			<div class="flex flex-row space-x-2">
+				<NumberField
+					bind:value={$form.incash}
+					name="incash"
+					label="Entré"
+					placeholder=""
+					isInValid={false}
+				/>
+				<NumberField
+					bind:value={$form.outcash}
+					name="outcash"
+					label="Sortie"
+					placeholder=""
+					isInValid={false}
+				/>
+			</div>
+		{/if}
 	</div>
 </div>
 <form use:enhance action="?/sell" class="mt-4 flex flex-col" method="POST">
