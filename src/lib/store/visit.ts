59e268ba8febf120d58477c74d8a75f7f4f9c8c4
Khaled FERJANI <kferjani@linagora.com>
2024-03-08 17:00:32 +0100

    🎨 feat: added visit items store

--- src/lib/store/visit.ts
+++ src/lib/store/visit.ts
@@ -2,7 +2,9 @@ import { writable } from 'svelte/store';
 import type { SuperValidated } from 'sveltekit-superforms';
 import type { addVisitSchema, updateVisitSchema } from '../schemas/visit';
 import type { removeSchema } from '../schemas';
+import type { Visit } from '$types';
 
+export const visitItems = writable<Visit[]>([]);
 export const addVisitFormStore = writable<SuperValidated<typeof addVisitSchema>>();
 export const updateVisitFormStore = writable<SuperValidated<typeof updateVisitSchema>>();
 export const deleteVisitFormStore = writable<SuperValidated<typeof removeSchema>>();
