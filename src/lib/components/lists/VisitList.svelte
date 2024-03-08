3a3f2b26e03be824fcf2713ef23fc39fc63fc0fe
Khaled FERJANI <kferjani@linagora.com>
2024-03-08 17:00:48 +0100

    💄 feat: updated visit list ui

--- src/lib/components/lists/VisitList.svelte
+++ src/lib/components/lists/VisitList.svelte
@@ -1,17 +1,23 @@
 <script lang="ts">
 	import Modal from '$lib/components/Modal.svelte';
 	import { enhance } from '$app/forms';
-	import ConfirmationDialog from '../ConfirmationDialog.svelte';
-	import AddVisitForm from '../forms/visit/AddVisitForm.svelte';
-	import type { VisitsResponse } from '$root/types';
+	import ConfirmationDialog from '$components/ConfirmationDialog.svelte';
+	import AddVisitForm from '$components/forms/visit/AddVisitForm.svelte';
+	import type { VisitStatusFilter as StatusFilter, Visit } from '$types';
 	import { formatDateString } from '$lib/utils/date';
+	import { visitItems } from '$store/visit';
 
 	export let isNew: boolean = false;
 
 	let openAddModal = isNew;
 	let deleteFormRef: HTMLFormElement;
-	let selectedItem: VisitsResponse | null;
+	let selectedItem: Visit | null;
 	let showConfirmation = false;
+	let statusFilter: StatusFilter = 'all';
+	let search: string;
+	let page = 0;
+
+	$: totalPages = Math.max(Math.ceil($visitItems.length / 10), 1);
 
 	$: handler = () => {
 		deleteFormRef.requestSubmit();
@@ -20,39 +26,25 @@
 		showConfirmation = false;
 	};
 
-	const remove = (item: VisitsResponse) => {
+	const remove = (item: Visit) => {
 		selectedItem = item;
 		showConfirmation = true;
 	};
 
-	export let data:
-		| ({
-				Bill: {
-					id: string;
-					visitId: string;
-					paid: boolean;
-					method: string;
-				} | null;
-				clinical_exams: {
-					id: string;
-					name: string;
-					price: number;
-					code: string;
-				}[];
-				surgical_acts: {
-					id: string;
-					name: string;
-					price: number;
-					code: string;
-				}[];
-				medical_acts: {
-					id: string;
-					name: string;
-					price: number;
-					code: string;
-				}[];
-		  } & VisitsResponse)[]
-		| [];
+	$: items = $visitItems.filter((item) => {
+		if (search && search.length) {
+			const searchString = search.toLocaleLowerCase();
+
+			if (
+				!item.motif.toLocaleLowerCase().includes(searchString) &&
+				!item.animal.name.toLocaleLowerCase().includes(searchString)
+			) {
+				return false;
+			}
+
+			return true;
+		}
+	});
 </script>
 
 <form use:enhance action="?/deleteVisit" method="POST" class="hidden" bind:this={deleteFormRef}>
@@ -81,43 +73,138 @@
 </Modal>
 <div class="flex flex-col items-center justify-start xl:pl-14 w-full">
 	<div
-		class="w-full xl:w-11/12 px-1 pt-10 lg:p-10 bg-white shadow-2xl border-gray-200 h-screen xl:h-fit xl:rounded-2xl"
+		class="w-full xl:w-11/12 px-1 pt-10 lg:p-10 bg-white shadow-2xl border-gray-200 h-screen xl:h-fit xl:rounded"
 	>
 		<div class="flex items-center gap-x-3 w-full">
 			<div class="w-full grow flex items-center justify-center gap-x-3 xl:px-1 xl:justify-start">
 				<h2 class="text-lg font-medium text-gray-800 dark:text-white">Visites</h2>
 
-				<span class="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full">{data.length}</span>
+				<span class="px-3 py-1 text-xs text-blue-600 bg-blue-100 rounded-full">{items.length}</span>
 			</div>
 			<button
-				class="flex-shrink hover:bg-gray-300 p-2 rounded-full flex space-x-5 flex-row"
 				on:click={() => (openAddModal = true)}
+				class="flex items-center justify-center w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-blue-500 rounded-lg shrink-0 sm:w-auto gap-x-2 hover:bg-blue-600 dark:hover:bg-blue-500 dark:bg-blue-600"
 			>
-				Ajouter
 				<svg
-					fill="none"
-					class="w-6 h-6 text-gray-700 dark:text-gray-300"
-					stroke="currentColor"
-					stroke-width="1.5"
-					viewBox="0 0 24 24"
 					xmlns="http://www.w3.org/2000/svg"
-					aria-hidden="true"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-5 h-5"
 				>
 					<path
 						stroke-linecap="round"
 						stroke-linejoin="round"
-						d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z"
+						d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
 					/>
 				</svg>
+				<span>Ajouter</span>
 			</button>
 		</div>
+		<div
+			class="flex px-1 py-2 lg:px-0 flex-col lg:flex-row items-start lg:items-center space-y-10 lg:space-y-0 justify-between"
+		>
+			<div
+				class="flex flex-row overflow-hidden bg-white border divide-x rounded-lg dark:bg-gray-900 rtl:flex-row-reverse dark:border-gray-700 dark:divide-gray-700"
+			>
+				<button
+					on:click={() => {
+						statusFilter = 'all';
+						page = 0;
+					}}
+					class="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 {statusFilter ===
+					'all'
+						? 'bg-gray-100'
+						: ''} sm:text-sm dark:bg-gray-800 dark:text-gray-300"
+				>
+					Tout
+				</button>
+				<button
+					on:click={() => {
+						statusFilter = 'completed';
+						page = 0;
+					}}
+					class="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 {statusFilter ===
+					'completed'
+						? 'bg-gray-100'
+						: ''} sm:text-sm dark:bg-gray-800 dark:text-gray-300 hover:bg-gray-100"
+				>
+					payés
+					<span
+						class="inline-flex items-center justify-center w-5 h-5 ms-2 text-xs font-semibold text-slate-800 bg-slate-200 rounded-full"
+					>
+						2
+					</span>
+				</button>
+				<button
+					on:click={() => {
+						statusFilter = 'partial';
+						page = 0;
+					}}
+					class="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 {statusFilter ===
+					'partial'
+						? 'bg-gray-100'
+						: ''} sm:text-sm dark:bg-gray-800 dark:text-gray-300 hover:bg-gray-100"
+				>
+					Arriarés
+					<span
+						class="inline-flex items-center justify-center w-5 h-5 ms-2 text-xs font-semibold text-slate-800 bg-slate-200 rounded-full"
+					>
+						1
+					</span>
+				</button>
+				<button
+					on:click={() => {
+						statusFilter = 'pending';
+						page = 0;
+					}}
+					class="px-5 py-2 text-xs font-medium text-gray-600 transition-colors duration-200 {statusFilter ===
+					'pending'
+						? 'bg-gray-100'
+						: ''} sm:text-sm dark:bg-gray-800 dark:text-gray-300 hover:bg-gray-100"
+				>
+					En attente
+					<span
+						class="inline-flex items-center justify-center w-5 h-5 ms-2 text-xs font-semibold text-slate-800 bg-slate-200 rounded-full"
+					>
+						1
+					</span>
+				</button>
+			</div>
 
+			<div class="flex items-center mt-0 h-6">
+				<span class="absolute">
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-5 h-5 mx-3 text-gray-400 dark:text-gray-600"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+						/>
+					</svg>
+				</span>
+
+				<input
+					bind:value={search}
+					type="text"
+					placeholder="Rechercher"
+					class="block w-full py-1.5 pr-5 text-gray-700 bg-white border border-gray-200 rounded-lg md:w-60 placeholder-gray-400/70 pl-11 rtl:pr-11 rtl:pl-5 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40"
+				/>
+			</div>
+		</div>
 		<div class="flex flex-col mt-6">
 			<div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
 				<div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-					<div class="overflow-hidden dark:border-gray-700 md:rounded-lg">
+					<div class="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
 						<table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-							<thead class="bg-gray-50 dark:bg-gray-800">
+							<thead class="bg-gray-50 dark:bg-gray-800 table-fixed">
 								<tr>
 									<th
 										scope="col"
@@ -149,7 +236,7 @@
 										scope="col"
 										class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400"
 									>
-										Payé
+										statut de paiement
 									</th>
 									<th scope="col" class="relative py-3.5 px-4">
 										<span class="sr-only">Modifier</span>
@@ -159,7 +246,7 @@
 							<tbody
 								class="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900"
 							>
-								{#each data as visit}
+								{#each items as visit}
 									<tr>
 										<td class="px-4 py-4 text-sm font-medium text-gray-700 whitespace-nowrap">
 											<div class="inline-flex items-center gap-x-3">
@@ -187,7 +274,7 @@
 											>{visit.medical_acts.length}</td
 										>
 										<td class="px-4 py-4 text-sm whitespace-nowrap">
-											{visit.Bill?.paid ? 'oui' : 'non'}
+											<!-- {visit.Bill?.paid ? 'oui' : 'non'} -->
 										</td>
 										<td class="px-4 py-4 text-sm whitespace-nowrap">
 											<div class="flex items-end justify-end gap-x-6 w-full">
@@ -242,57 +329,70 @@
 			</div>
 		</div>
 
-		<div class="flex items-center justify-between mt-6">
-			<a
-				href="#"
-				class="flex items-center px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800"
-			>
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="w-5 h-5 rtl:-scale-x-100"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
-					/>
-				</svg>
-
-				<span> précédent </span>
-			</a>
-
-			<div class="items-center hidden lg:flex gap-x-3">
-				<a
-					href="#"
-					class="px-2 py-1 text-sm text-blue-500 rounded-md dark:bg-gray-800 bg-blue-100/60">1</a
+		<div class="mt-6 pb-5 lg:pb-0 sm:flex sm:items-center sm:justify-between">
+			<div class="text-sm text-gray-500 dark:text-gray-400">
+				Page <span class="font-medium text-gray-700 dark:text-gray-100"
+					>{page + 1} sur {totalPages}</span
 				>
 			</div>
 
-			<a
-				href="#"
-				class="flex items-center px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800"
-			>
-				<span> Suivant </span>
-
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					class="w-5 h-5 rtl:-scale-x-100"
+			<div class="flex items-center mt-4 gap-x-4 sm:mt-0">
+				<button
+					on:click={() => (page -= 1)}
+					disabled={page <= 0}
+					class="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 {page <=
+					0
+						? 'bg-slate-200'
+						: 'bg-white'} border rounded-md sm:w-auto gap-x-2 {page <= 0
+						? 'hover:bg-slate-200'
+						: 'hover:bg-gray-100'}  dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800"
 				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-					/>
-				</svg>
-			</a>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-5 h-5 rtl:-scale-x-100"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18"
+						/>
+					</svg>
+
+					<span> précédent </span>
+				</button>
+
+				<button
+					disabled={page >= totalPages - 1}
+					on:click={() => (page += 1)}
+					class="flex items-center justify-center w-1/2 px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 {page >=
+					totalPages - 1
+						? 'bg-slate-200'
+						: 'bg-white'} border rounded-md sm:w-auto gap-x-2 {page >= totalPages - 1
+						? 'hover:bg-slate-200'
+						: 'hover:bg-gray-100'} dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800"
+				>
+					<span> Suivant </span>
+
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-5 h-5 rtl:-scale-x-100"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
+						/>
+					</svg>
+				</button>
+			</div>
 		</div>
 	</div>
 </div>
