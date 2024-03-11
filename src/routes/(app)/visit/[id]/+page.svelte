f2829844c36524bd525172ebcb1786c0e4963a8a
Khaled FERJANI <kferjani@linagora.com>
2024-03-11 21:14:13 +0100

    🎨 feat: update page load init to include bill

--- src/routes/(app)/visit/[id]/+page.svelte
+++ src/routes/(app)/visit/[id]/+page.svelte
@@ -2,5 +2,5 @@
 	import type { PageData } from './$types';
 	export let data: PageData;
 
-	$: ({ visit, clinicalExams, medicalActs, surgicalActs, form } = data);
+	$: ({ visit, clinicalExams, medicalActs, surgicalActs, form, bill } = data);
 </script>
