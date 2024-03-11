857e055213863bd40b27098de2afdaa25e7c5e2a
Khaled FERJANI <kferjani@linagora.com>
2024-03-11 13:21:24 +0100

    ✨ feat: added payment status component

---
+++ src/lib/components/dispaly/PaymentStatus.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+	export let paid: boolean = false;
+	export let total: number = 0;
+	export let total_paid: number = 0;
+
+	$: partiallyPaid = total_paid > 0 && total_paid < total;
+	$: pendingPayment = total_paid === 0;
+</script>
+
+<div class="flex items-center gap-x-4 w-full">
+	<span
+		class="flex font-semibold items-center justify-center text-white w-full py-2 p-1 {paid
+			? 'bg-emerald-500/80'
+			: pendingPayment
+			? 'bg-red-500/80'
+			: partiallyPaid
+			? 'bg-orange-500/80'
+			: ''} rounded-lg shrink-0"
+	>
+		{#if paid}
+			Payé
+		{:else if partiallyPaid}
+			Arriaré
+		{:else}
+			Impayé
+		{/if}
+	</span>
+</div>
