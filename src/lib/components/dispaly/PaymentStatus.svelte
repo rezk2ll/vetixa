ebfb1eb97647b844cf54eb1a4b83391b4854a554
Khaled FERJANI <kferjani@linagora.com>
2024-03-11 14:13:18 +0100

    💄 feat: updated payment status indicator

--- src/lib/components/dispaly/PaymentStatus.svelte
+++ src/lib/components/dispaly/PaymentStatus.svelte
@@ -10,13 +10,13 @@
 
 <div class="flex items-center gap-x-4 w-full">
 	<span
-		class="flex font-semibold items-center justify-center text-white w-full py-2 p-1 {paid
+		class="flex font-semibold items-center justify-center text-white w-full p-2 {paid
 			? 'bg-emerald-500/80'
 			: pendingPayment
 			? 'bg-red-500/80'
 			: partiallyPaid
 			? 'bg-orange-500/80'
-			: ''} rounded-lg shrink-0"
+			: ''} rounded shrink-0"
 	>
 		{#if paid}
 			Payé
