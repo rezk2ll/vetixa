cb0a1e94ceb45b8ba8a68004004c53e19d74341b
Khaled FERJANI <kferjani@linagora.com>
2024-03-14 13:53:31 +0100

    🎨 feat: populate visit stores from page load

--- src/routes/(app)/visit/[id]/+page.svelte
+++ src/routes/(app)/visit/[id]/+page.svelte
@@ -1,10 +1,25 @@
 <script lang="ts">
 	import PaymentTab from '$components/tabs/visit/PaymentTab.svelte';
 	import TabContainer from '$components/tabs/visit/VisitTabContainer.svelte';
+	import { addVisitExamFormStore, currentVisit, removeVisitItemFormStore } from '$store/visit';
+	import {
+		clinicalExams as clinicalExamsStore,
+		medicalActs as medicalActsStore,
+		surgivalActs as surgivalActsStore
+	} from '$store/acts';
 	import type { PageData } from './$types';
+
 	export let data: PageData;
 
-	$: ({ visit, clinicalExams, medicalActs, surgicalActs, form, bill } = data);
+	$: ({ visit, clinicalExams, medicalActs, surgicalActs, form, bill, addExamForm, removeExamForm } =
+		data);
+
+	$: currentVisit.set(visit);
+	$: addVisitExamFormStore.set(addExamForm);
+	$: removeVisitItemFormStore.set(removeExamForm);
+	$: medicalActsStore.set(medicalActs);
+	$: clinicalExamsStore.set(clinicalExams);
+	$: surgivalActsStore.set(surgicalActs);
 </script>
 
 <div class="flex flex-col xl:flex-row lg:pl-14 w-full">
