52b8e116e7c97be978e8969cc3751aa3cb5e2f48
Khaled FERJANI <kferjani@linagora.com>
2024-05-14 16:54:28 +0100

    🎨 feat: update funds transaction form schema
    add payment method details

--- src/lib/schemas/funds.ts
+++ src/lib/schemas/funds.ts
@@ -3,8 +3,12 @@ import { z } from 'zod';
 export const addFundsSchema = z.object({
 	amount: z
 		.number()
-		.positive()
+		.default(0)
+		.or(z.string().regex(/\d+/).transform(Number))
 		.default(0)
 		.refine((n) => n > 0),
-	description: z.string().optional().default('')
+	method: z.string().min(1).default('cash'),
+	incash: z.number().min(0).default(0).or(z.string().regex(/\d+/).transform(Number)).default(0),
+	outcash: z.number().min(0).default(0).or(z.string().regex(/\d+/).transform(Number)).default(0),
+	description: z.string()
 });
