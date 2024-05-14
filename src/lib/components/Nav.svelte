b63575f49ec3fd6651ca9ceed81a182891ccd5e6
Khaled FERJANI <kferjani@linagora.com>
2024-05-14 17:17:33 +0100

    ✨ feat: add logout button title

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -271,7 +271,7 @@
 			use:enhance
 			class="p-1.5 text-gray-700 focus:outline-nones transition-colors flex items-center justify-center duration-200 rounded-lg dark:text-gray-200 dark:bg-gray-800 hover:bg-gray-100"
 		>
-			<button type="submit">
+			<button type="submit" title="Se déconnecter">
 				<svg
 					fill="none"
 					stroke="currentColor"
