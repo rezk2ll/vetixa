19f56d1ab920017bb0e2adaea953d7d3bd9c1974
Khaled FERJANI <kferjani@linagora.com>
2024-05-20 16:55:31 +0100

    ✨ feat: added search store

--- src/lib/store/search.ts
+++ src/lib/store/search.ts
@@ -1,3 +1,5 @@
+import type { SearchResultInfo } from '$types';
 import { writable } from 'svelte/store';
 
 export const searchOpen = writable<boolean>(false);
+export const searchPage = writable<SearchResultInfo>();
