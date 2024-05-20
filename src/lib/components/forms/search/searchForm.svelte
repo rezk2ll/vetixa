f9e9e1895db91fdeeaf58997c56059b2826b1985
Khaled FERJANI <kferjani@linagora.com>
2024-05-20 16:56:08 +0100

    ✨ feat: add search buttons

--- src/lib/components/forms/search/searchForm.svelte
+++ src/lib/components/forms/search/searchForm.svelte
@@ -1,57 +1,77 @@
 <script lang="ts">
-	import { searchOpen } from '$store/search';
 	import { onMount } from 'svelte';
+	import { searchOpen } from '$store/search';
+	import type { SearchEntityType } from '$types';
+	import { browser } from '$app/environment';
+	import { goto } from '$app/navigation';
+	import PrimaryButton from '$components/buttons/PrimaryButton.svelte';
+	import RemoveButton from '$components/buttons/removeButton.svelte';
 
 	const closeSearch = () => {
+		value = '';
 		searchOpen.set(false);
 	};
 
 	let inputRef: HTMLInputElement;
 	let value: string = '';
-	let searchOptions: 'animal' | 'client' = 'client';
+	let searchOptions: SearchEntityType = 'client';
 	let clientPlaceholder = 'Rechercher par nom, email, téléphone, addresse...';
 	let animalPlaceholder = 'Rechercher par nom, identifiant unique, race, espèce...';
 
 	onMount(async () => {
 		inputRef && inputRef.focus();
 	});
+
+	const search = () => {
+		if (!browser || value.length < 2) return;
+
+		const url = new URL('/search', window.location.origin);
+
+		url.searchParams.set('query', value);
+		url.searchParams.set('target', searchOptions);
+
+		goto(url);
+		$searchOpen = false;
+	};
 </script>
 
 <div class="flex flex-col gap-6">
-	<button type="button" class="relative flex items-center w-full">
-		<span class="absolute text-gray-400 left-3"
-			><svg
-				xmlns="http://www.w3.org/2000/svg"
-				fill="none"
-				viewBox="0 0 24 24"
-				stroke-width="1.5"
-				stroke="currentColor"
-				class="w-5 h-5"
-				><path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-				/></svg
-			></span
-		><input
-			bind:this={inputRef}
-			type="text"
-			placeholder={searchOptions === 'client' ? clientPlaceholder : animalPlaceholder}
-			bind:value
-			class="block py-4 grow pl-14 pr-16 text-blue-950 placeholder-gray-400/70 focus:outline-none"
-		/>
-		<button
-			type="button"
-			on:click={closeSearch}
-			class="absolute inset-y-0 flex items-center right-3"
-		>
-			<kbd
-				class="inline-flex items-center px-1 py-1 font-sans text-xs text-gray-500 border rounded-md dark:text-gray-400 dark:border-gray-700"
+	<form on:submit|preventDefault={search}>
+		<button type="button" class="relative flex items-center w-full">
+			<span class="absolute text-gray-400 left-3"
+				><svg
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke-width="1.5"
+					stroke="currentColor"
+					class="w-5 h-5"
+					><path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+					/></svg
+				></span
+			><input
+				bind:this={inputRef}
+				type="text"
+				placeholder={searchOptions === 'client' ? clientPlaceholder : animalPlaceholder}
+				bind:value
+				class="block py-4 grow pl-14 pr-16 text-blue-950 placeholder-gray-400/70 focus:outline-none"
+			/>
+			<button
+				type="button"
+				on:click={closeSearch}
+				class="absolute inset-y-0 flex items-center right-3"
 			>
-				Esc
-			</kbd>
+				<kbd
+					class="inline-flex items-center px-1 py-1 font-sans text-xs text-gray-500 border rounded-md dark:text-gray-400 dark:border-gray-700"
+				>
+					Esc
+				</kbd>
+			</button>
 		</button>
-	</button>
+	</form>
 	<div class="w-full px-3">
 		<div class="flex w-full gap-2 rounded-3xl bg-gray-200">
 			<div class="w-full">
@@ -87,5 +107,9 @@
 				>
 			</div>
 		</div>
+	</div>
+	<div class="flex gap-1 p-1">
+		<PrimaryButton handler={search}>Rechercher</PrimaryButton>
+		<RemoveButton handler={closeSearch}>Annuler</RemoveButton>
 	</div>
 </div>
