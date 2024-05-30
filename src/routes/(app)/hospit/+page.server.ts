33665541f715a68df924c547330856c9148e812b
Khaled FERJANI <kferjani@linagora.com>
2024-05-30 22:24:25 +0100

    ✨ feat(hospit): added count and status filter to route

--- src/routes/(app)/hospit/+page.server.ts
+++ src/routes/(app)/hospit/+page.server.ts
@@ -1,13 +1,30 @@
-import type { CagesResponse, Hospit, HospitalisationResponse, Treatment } from '$types';
+import type {
+  HospitCompletedViewResponse,
+  HospitPendingViewResponse,
+  CagesResponse,
+  Hospit,
+  HospitStatusFilter,
+  HospitalisationResponse,
+  Treatment
+} from '$types';
 import type { RecordModel } from 'pocketbase';
 import type { PageServerLoad } from './$types';
 
 export const load = (async ({ locals: { pb }, url: { searchParams } }) => {
 	const page = parseInt(searchParams.get('page') ?? '1');
 	const query = searchParams.get('query') ?? '';
+	const filter = (searchParams.get('filter') as HospitStatusFilter) ?? 'all';
+
+	const collection =
+		filter === 'pending'
+			? 'hospit_pending_list'
+			: filter === 'complete'
+			? 'hospit_completed_list'
+			: 'hospitalisation';
 
 	const cages = await pb.collection('cages').getFullList<CagesResponse>();
-	const hospit = await pb.collection('hospitalisation').getList<HospitalisationResponse>(page, 10, {
+	const hospit = await pb.collection(collection).getList<HospitalisationResponse>(page, 10, {
+		sort: '-updated',
 		filter: `note ~ "${query}" || cage.code ~ "${query}" || visit.animal.name ~ "${query}" || visit.animal.identifier ~ "${query}" || visit.animal.client.name ~ "${query}" || visit.animal.client.tel ~ "${query}"`,
 		expand: 'visit, visit.animal, visit.animal.client, cage'
 	});
@@ -27,12 +44,25 @@ export const load = (async ({ locals: { pb }, url: { searchParams } }) => {
 		} satisfies Hospit;
 	});
 
+	const pendingCount = await pb
+		.collection('hospit_pending_view')
+		.getOne<HospitPendingViewResponse>('pending');
+
+	const completedCount = await pb
+		.collection('hospit_completed_view')
+		.getOne<HospitCompletedViewResponse>('completed');
+
 	return {
 		cages,
 		pageInfo: {
 			...hospit,
 			items,
-			query
+			query,
+			filter,
+			count: {
+				pending: pendingCount.total ?? 0,
+				completed: completedCount.total ?? 0
+			}
 		}
 	};
 }) satisfies PageServerLoad;
