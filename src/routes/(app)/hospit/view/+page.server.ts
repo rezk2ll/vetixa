6261028423360ad5363befa2a6482a84ac387011
Khaled FERJANI <kferjani@linagora.com>
2024-06-01 23:10:12 +0100

    🎨 feat: add hospit view route

--- src/routes/(app)/hospit/view/+page.server.ts
+++ src/routes/(app)/hospit/view/+page.server.ts
@@ -1,10 +1,39 @@
-import type { CagesResponse } from '$types';
+import type { CageItem, CagesResponse, HospitPendingListResponse, Treatment } from '$types';
+import type { RecordModel } from 'pocketbase';
 import type { PageServerLoad } from './$types';
 
 export const load = (async ({ locals: { pb } }) => {
 	const cages = await pb.collection('cages').getFullList<CagesResponse>();
+	const hospitList = await pb
+		.collection('hospit_pending_list')
+		.getFullList<HospitPendingListResponse>({
+			expand: 'visit, visit.animal, visit.animal.client, cage'
+		});
 
+	const cageList: CageItem[] = cages.map((cage) => {
+		const matchingHospit = hospitList.find((item) => item.cage === cage.id);
+
+		if (!matchingHospit) {
+			return cage;
+		}
+
+		return {
+			...cage,
+			hospit: {
+				...matchingHospit,
+				cage,
+				treatment: (matchingHospit.treatment as Treatment[]) || [],
+				visit: {
+					...((matchingHospit.expand as RecordModel)?.visit || {}),
+					animal: {
+						...((matchingHospit.expand as RecordModel)?.visit.expand.animal || {})
+					},
+					client: (matchingHospit.expand as RecordModel)?.visit.expand.animal.expand.client || {}
+				}
+			}
+		} satisfies CageItem;
+	});
 	return {
-		cages
+		cageList
 	};
 }) satisfies PageServerLoad;
--- src/routes/(app)/hospit/view/+page.svelte
+++ src/routes/(app)/hospit/view/+page.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import { cagesList } from '$store/hospit';
+	import { cagesInfo } from '$store/hospit';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 
-	$: ({ cages } = data);
-  $: cagesList.set(cages);
+	$: ({ cageList } = data);
+	$: cagesInfo.set(cageList);
 </script>
