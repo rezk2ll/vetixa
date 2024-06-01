1a336f7609ee3502799c406d59d0ff0883b0573b
Khaled FERJANI <kferjani@linagora.com>
2024-06-01 23:09:58 +0100

    ✨ feat: add cage info store

--- src/lib/store/hospit.ts
+++ src/lib/store/hospit.ts
@@ -1,11 +1,13 @@
 import { writable } from 'svelte/store';
-import type { HospitPageInfo, CagesResponse, HospitalisationResponse } from '$types';
+import type { HospitPageInfo, CagesResponse, HospitalisationResponse, CageItem } from '$types';
 import type { removeSchema } from '$lib/schemas';
 import type { updateVisitHospitalisationSchema } from '$lib/schemas/visit';
 import type { SuperValidated, Infer } from 'sveltekit-superforms';
 
 export const hospitList = writable<HospitalisationResponse[]>([]);
 export const cagesList = writable<CagesResponse[]>([]);
+export const cagesInfo = writable<CageItem[]>([]);
+
 export const updateVisitHospitalisationFormStore =
 	writable<SuperValidated<Infer<typeof updateVisitHospitalisationSchema>>>();
 export const removeVisitHospitalisationFormStore =
