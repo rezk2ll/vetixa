4d870d5d7fc332044d0c6d9c785de326708562c8
Khaled FERJANI <kferjani@linagora.com>
2024-06-05 11:22:42 +0100

    🍱 feat: added hospit related icons

---
+++ src/lib/components/icons/ForwardArrow.svelte
@@ -0,0 +1,16 @@
+<svg
+	data-slot="icon"
+	fill="none"
+	stroke-width="1.5"
+	stroke="currentColor"
+	viewBox="0 0 24 24"
+	class="w-5 h-5"
+	xmlns="http://www.w3.org/2000/svg"
+	aria-hidden="true"
+>
+	<path
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
+	/>
+</svg>
