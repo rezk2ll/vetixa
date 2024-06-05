605aa67535156773b8b1dbf28e983d64f3da1880
Khaled FERJANI <kferjani@linagora.com>
2024-06-05 11:22:53 +0100

    ✨ feat: added empty hospit component

---
+++ src/lib/components/display/cages/EmptyCage.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import House from '$components/icons/House.svelte';
+</script>
+
+<div class="flex flex-col justify-center items-center gap-2 h-full relative">
+	<House />
+	<div
+		class="flex flex-row items-center justify-center capitalize bg-gray-600 text-slate-200 text-base font-semibold py-1.5 px-5 top-0 w-full mt-auto rounded"
+	>
+		cage vide !
+	</div>
+</div>
