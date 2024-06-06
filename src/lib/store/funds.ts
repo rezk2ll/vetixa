7bf2342feebdbedfc5fbd4ebda7b2f475e8eb661
Khaled FERJANI <kferjani@linagora.com>
2024-06-06 17:13:18 +0100

    ✨ feat: add fund page info store

--- src/lib/store/funds.ts
+++ src/lib/store/funds.ts
@@ -1,10 +1,9 @@
 import type { addFundsSchema } from '$lib/schemas';
 import { writable } from 'svelte/store';
 import type { SuperValidated } from 'sveltekit-superforms';
-import type { Fund } from '$types';
+import type { FundsPageInfo } from '$types';
 import type { Infer } from 'sveltekit-superforms';
 
 export const addFundsFormStore = writable<SuperValidated<Infer<typeof addFundsSchema>>>();
 export const addExpenseFormStore = writable<SuperValidated<Infer<typeof addFundsSchema>>>();
-export const fundItems = writable<Fund[]>([]);
-
+export const fundsPageInfo = writable<FundsPageInfo>();
