993c28041e3f292fffb43c6c9551de4c683c58c3
Khaled FERJANI <kferjani@linagora.com>
2024-06-07 13:52:51 +0100

    ✨ feat: added clients page info store

--- src/lib/store/clients.ts
+++ src/lib/store/clients.ts
@@ -1,12 +1,10 @@
 import { writable } from 'svelte/store';
 import type { addClientSchema, removeSchema, updateClientSchema } from '$lib/schemas';
 import type { SuperValidated } from 'sveltekit-superforms';
-import type { ClientsResponseData } from '$types';
+import type { ClientsPageInfo } from '$types';
 import type { Infer } from 'sveltekit-superforms';
-
 
 export const addClientFormStore = writable<SuperValidated<Infer<typeof addClientSchema>>>();
 export const updateClientFormStore = writable<SuperValidated<Infer<typeof updateClientSchema>>>();
 export const removeClientFormStore = writable<SuperValidated<Infer<typeof removeSchema>>>();
-
-export const clients = writable<ClientsResponseData>([]);
+export const clientsPageInfo = writable<ClientsPageInfo>();
