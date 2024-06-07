cd964b3af60d4915954f9193a9f1884a8cc3ba1e
Khaled FERJANI <kferjani@linagora.com>
2024-06-07 16:16:40 +0100

    ✨ feat: added animals page info store

--- src/lib/store/animals.ts
+++ src/lib/store/animals.ts
@@ -1,5 +1,5 @@
 import { writable } from 'svelte/store';
-import type { AnimalsResponse } from '$types';
+import type { AnimalsPageInfo, AnimalsResponse } from '$types';
 import type { SuperValidated } from 'sveltekit-superforms';
 import type { addAnimalSchema, removeSchema, updateAnimalSchema } from '$lib/schemas';
 import type { Infer } from 'sveltekit-superforms';
@@ -8,3 +8,4 @@ export const animals = writable<AnimalsResponse[]>([]);
 export const addAnimalFormStore = writable<SuperValidated<Infer<typeof addAnimalSchema>>>();
 export const updateAnimalFormStore = writable<SuperValidated<Infer<typeof updateAnimalSchema>>>();
 export const deleteAnimalFormStore = writable<SuperValidated<Infer<typeof removeSchema>>>();
+export const animalsPageInfo = writable<AnimalsPageInfo>();
