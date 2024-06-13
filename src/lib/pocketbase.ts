aa6be934586364f87c426120d5a7b3ab30b58ac1
Khaled FERJANI <kferjani@linagora.com>
2024-06-13 11:21:54 +0100

    🎨 chore: use dynamic Pocketbase url env variable

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,7 +1,7 @@
-import { PUBLIC_POCKETBASE_URL } from '$env/static/public';
+import { env } from '$env/dynamic/public';
 import PocketBase from 'pocketbase';
 import type { TypedPocketBase } from '$types';
 
-export const createInstance = () => new PocketBase(PUBLIC_POCKETBASE_URL) as TypedPocketBase;
+export const createInstance = () => new PocketBase(env.PUBLIC_POCKETBASE_URL) as TypedPocketBase;
 
 export default createInstance();
