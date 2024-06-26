f0a561c1c0376429117ada4290b246e83d2ac919
Khaled Ferjani <kferjani@linagora.com>
2024-06-26 15:17:49 +0100

    🐛 fix: client animals list view on mobile (#77)

--- src/lib/components/lists/ClientAnimalsList.svelte
+++ src/lib/components/lists/ClientAnimalsList.svelte
@@ -273,7 +273,7 @@
 			<div class="flex flex-col mt-6">
 				<div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
 					<div class="min-w-full py-2 align-middle md:px-6 lg:px-8">
-						<div class="overflow-hidden border border-gray-200 md:rounded-lg">
+						<div class="border border-gray-200 md:rounded-lg">
 							<table class="min-w-full divide-y divide-gray-200 table-fixed">
 								<thead class="bg-slate-50">
 									<tr>
