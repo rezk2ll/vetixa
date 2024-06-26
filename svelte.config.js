84a6510b41b448bf733e81a29dd0de85f1dde1c8
Khaled FERJANI <kferjani@linagora.com>
2024-06-26 10:57:42 +0100

    🚨 chore: lint

--- svelte.config.js
+++ svelte.config.js
@@ -6,23 +6,23 @@ const adapter = process.env.ADAPTER === 'node' ? adapterNode() : adapterAuto();
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
-  preprocess: vitePreprocess(),
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
 
-  kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter,
-    alias: {
-      $components: 'src/lib/components',
-      $lib: 'src/lib',
-      $store: 'src/lib/store',
-      $utils: 'src/lib/utils',
-      $types: 'src/types'
-    }
-  }
+	kit: {
+		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
+		adapter,
+		alias: {
+			$components: 'src/lib/components',
+			$lib: 'src/lib',
+			$store: 'src/lib/store',
+			$utils: 'src/lib/utils',
+			$types: 'src/types'
+		}
+	}
 };
 
 export default config;
