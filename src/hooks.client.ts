540cf828d74c170ba378d16102a2728eb97adb31
Khaled Ferjani <kferjani@linagora.com>
2024-06-27 11:19:15 +0100

    🐛 fix: issue with cookies not being set while logging in (#84)

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,9 +1,13 @@
 import pb from '$lib/pocketbase';
-import { currentUser } from '$lib/store/user';
+import { currentUser } from '$store/user';
 
 pb.authStore.loadFromCookie(document.cookie);
 
 pb.authStore.onChange(() => {
 	currentUser.set(pb.authStore.model);
-	document.cookie = pb.authStore.exportToCookie({ httpOnly: false });
+	document.cookie = pb.authStore.exportToCookie({
+		httpOnly: false,
+		sameSite: 'Lax',
+		secure: false
+	});
 }, true);
