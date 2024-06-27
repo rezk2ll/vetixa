5216af353dc041f67cf6de35412852df72d75d8a
Khaled FERJANI <kferjani@linagora.com>
2024-06-27 11:15:59 +0100

    🐛 fix: issue with cookies not being set while logging in

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,26 +2,31 @@ import { createInstance } from '$lib/pocketbase';
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	const pb = createInstance();
-
-	pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
+	event.locals.pb = createInstance();
+	event.locals.pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
 
 	try {
-		if (pb.authStore.isValid) {
-			await pb.collection('users').authRefresh();
+		if (event.locals.pb.authStore.isValid) {
+			await event.locals.pb.collection('users').authRefresh();
 		}
 	} catch (_) {
-		pb.authStore.clear();
+		event.locals.pb.authStore.clear();
 		console.log(_);
 	}
 
-	pb.autoCancellation(false);
-	event.locals.pb = pb;
-	event.locals.user = pb.authStore.model;
+	event.locals.pb.autoCancellation(false);
+	event.locals.user = event.locals.pb.authStore.model;
 
 	const response = await resolve(event);
 
-	response.headers.set('set-cookie', pb.authStore.exportToCookie({ httpOnly: false }));
+	response.headers.append(
+		'set-cookie',
+		event.locals.pb.authStore.exportToCookie({
+			httpOnly: false,
+			sameSite: 'Lax',
+			secure: false
+		})
+	);
 
 	return response;
 };
