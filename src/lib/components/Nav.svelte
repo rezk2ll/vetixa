0daa2b2005eededb69c8f43d392798699afc904d
Khaled FERJANI <kferjani@linagora.com>
2024-06-28 16:10:07 +0100

    ✨ feat: use pocketbase admin url env variable

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -281,7 +281,7 @@
 	<div class="flex flex-col space-y-6">
 		<a
 			target="_blank"
-			href={`${env.PUBLIC_POCKETBASE_URL}/_/`}
+			href={`${env.PUBLIC_POCKETBASE_ADMIN_URL}`}
 			title="Paramètres"
 			class="p-1.5 text-gray-700 focus:outline-nones transition-colors duration-200 rounded-lg dark:text-gray-200 dark:bg-gray-800 hover:bg-gray-100"
 		>
