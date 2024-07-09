1a2b5c7353bf55fca2714c8f63ddf92e13260f59
Khaled FERJANI <kferjani@linagora.com>
2024-07-09 15:02:42 +0100

    ✨ feat: added current animal store

--- src/lib/store/animals.ts
+++ src/lib/store/animals.ts
@@ -4,6 +4,7 @@ import type { SuperValidated } from 'sveltekit-superforms';
 import type { addAnimalSchema, removeSchema, updateAnimalSchema } from '$lib/schemas';
 import type { Infer } from 'sveltekit-superforms';
 
+export const currentAnimal = writable<AnimalsResponse>();
 export const animals = writable<AnimalsResponse[]>([]);
 export const addAnimalFormStore = writable<SuperValidated<Infer<typeof addAnimalSchema>>>();
 export const updateAnimalFormStore = writable<SuperValidated<Infer<typeof updateAnimalSchema>>>();
