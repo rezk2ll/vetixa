d5aa9eb1b79586fc61af5a9ef6ccc979452b6365
Khaled FERJANI <kferjani@linagora.com>
2024-07-14 01:54:05 +0100

    🎨 feat: fetch and store configuration

--- src/routes/(app)/+layout.server.ts
+++ src/routes/(app)/+layout.server.ts
@@ -1,8 +1,30 @@
 import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
+import { ConfigurationResponse } from '$types';
 
 export const load: LayoutServerLoad = async ({ locals }) => {
 	if (!locals.user) {
 		throw redirect(303, '/login');
 	}
+
+	let configuration: ConfigurationResponse | undefined;
+	let logo = '/logo.svg';
+
+	try {
+		configuration = await locals.pb
+			.collection('configuration')
+			.getFirstListItem<ConfigurationResponse>('');
+		if (configuration && configuration.logo) {
+			logo = locals.pb.files.getUrl(configuration, configuration.logo);
+		}
+	} catch (error) {
+		console.error(error);
+	}
+
+	return {
+		config: {
+			...configuration,
+			logo
+		}
+	};
 };
