4dec8d2edd37247dcf8ee43a1358d3dca326ebe3
Khaled FERJANI <kferjani@linagora.com>
2024-07-19 14:42:52 +0100

    🚨 chore: lint

--- src/lib/components/forms/agenda/AddEventForm.svelte
+++ src/lib/components/forms/agenda/AddEventForm.svelte
@@ -5,7 +5,7 @@
 	import TextAreaField from '../../inputs/TextAreaField.svelte';
 	import DateInput from 'date-picker-svelte/DateInput.svelte';
 	import { localeFromDateFnsLocale } from 'date-picker-svelte/locale';
-	import fr from 'date-fns/locale/fr/index'
+	import fr from 'date-fns/locale/fr/index';
 	import { formatISO } from 'date-fns';
 	import { toast } from 'svelte-sonner';
 	import DoubleArrow from '$components/icons/DoubleArrow.svelte';
