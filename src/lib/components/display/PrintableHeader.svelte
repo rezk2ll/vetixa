7e003f48a5917daa7b248e199c542bafd1f06356
Khaled FERJANI <kferjani@linagora.com>
2024-07-19 11:50:29 +0100

    💄 fix: report header style

--- src/lib/components/display/PrintableHeader.svelte
+++ src/lib/components/display/PrintableHeader.svelte
@@ -6,6 +6,6 @@
 
 <div>
 	<img src={config.logo} alt="logo" height="52" width="52" />
-	<h1 class="mt-2 text-lg md:text-xl font-semibold text-blue-600 uppercase">{config.name}</h1>
+	<h1 class="mt-1 text-lg md:text-xl font-semibold text-blue-600 uppercase">{config.name}</h1>
 	<dd class="col-span-2 text-gray-500">{config.facility}</dd>
 </div>
