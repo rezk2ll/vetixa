8d0f430ceecf5cbc0a8ed7ae9e1b1323ac3a9085
Khaled Ferjani <kferjani@linagora.com>
2024-07-22 02:42:50 +0100

    ✨ Cages sorting (#152)
    * ✨ feat: added cages sorting utility
    
    * 🐛 fix: use cage sorting to display cages

--- src/routes/(app)/hospit/view/+page.server.ts
+++ src/routes/(app)/hospit/view/+page.server.ts
@@ -10,6 +10,7 @@ import type { Actions, PageServerLoad } from './$types';
 import { setError, superValidate } from 'sveltekit-superforms';
 import { zod } from 'sveltekit-superforms/adapters';
 import { changeHospitColorsSchema } from '$lib/schemas/hospit';
+import { cageCompare } from '$utils/cage';
 
 export const load = (async ({ locals: { pb } }) => {
 	const cages = await pb.collection('cages').getFullList<CagesResponse>();
@@ -46,24 +47,7 @@ export const load = (async ({ locals: { pb } }) => {
 				}
 			} satisfies CageItem;
 		})
-		.sort((a, b) => {
-			const extractParts = (str: string): [string, number] => {
-				str = str.trim();
-				const alphaPart = str.match(/^[^\d]*/)?.[0] ?? '';
-				const numPart = str.match(/\d+$/)?.[0] ?? '0';
-				return [alphaPart, parseInt(numPart, 10)];
-			};
-
-			const [alphaA, numA] = extractParts(a.code.toLowerCase());
-			const [alphaB, numB] = extractParts(b.code.toLowerCase());
-
-			const alphaCompare = alphaA.localeCompare(alphaB, undefined, { sensitivity: 'base' });
-			if (alphaCompare !== 0) {
-				return alphaCompare;
-			}
-
-			return numA - numB;
-		});
+		.sort(cageCompare);
 
 	return {
 		cageList,
