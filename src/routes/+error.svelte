6b8c0b24c91508a734f91cf80542159dcb7981ab
Khaled FERJANI <kferjani@linagora.com>
2024-07-29 12:02:11 +0100

    ✨ feat: added root error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import CustomError from '$components/display/CustomError.svelte';
+	import '../app.css';
+</script>
+
+<div class="min-h-screen w-full bg-white overflow-hidden gradiant flex flex-col pt-10 xl:pt-16">
+	<CustomError code={$page.status} message={$page.error?.message} />
+</div>
