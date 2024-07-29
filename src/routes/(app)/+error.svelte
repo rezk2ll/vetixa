4f5e4d194141a870668199811b76046fdfc3c269
Khaled Ferjani <kferjani@linagora.com>
2024-07-29 12:06:55 +0100

    ✨ add custom error pages (#166)
    * ✨ feat: added custom error component
    
    * 🍱 feat: added error image asset
    
    * ✨ feat: added root error page
    
    * 🎨 feat: added custom 404 route
    
    * ✨ feat: added app error page

---
+++ src/routes/(app)/+error.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import CustomError from '$components/display/CustomError.svelte';
+</script>
+
+<CustomError code={$page.status} message={$page.error?.message} />
---
+++ src/routes/(app)/404/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import CustomError from '$lib/components/display/CustomError.svelte';
+</script>
+
+<CustomError />
