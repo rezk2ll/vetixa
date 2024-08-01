ad5aaf8aadedd8968c018798545ddb3647d0716e
Khaled FERJANI <kferjani@linagora.com>
2024-08-01 11:47:09 +0100

    ✨ feat: added print certificate button component

---
+++ src/lib/components/display/certificates/PrintCertificate.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import PrimaryButton from '$components/buttons/PrimaryButton.svelte';
+	import HealthCertificate from '$lib/components/icons/HealthCertificate.svelte';
+
+	export let handler: () => void = () => {};
+</script>
+
+<div class="flex flex-col gap-6">
+	<PrimaryButton full {handler}>
+		<div class="flex space-x-5 items-start justify-start">
+			<HealthCertificate />
+			<div class="text-base font-semibold grow">Certificat sanitaire international</div>
+		</div>
+	</PrimaryButton>
+</div>
