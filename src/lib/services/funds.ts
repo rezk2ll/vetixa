db67410fd2986f9b9ff9911f56fe28c0b13b3c2e
Khaled FERJANI <kferjani@linagora.com>
2024-08-01 13:18:00 +0100

    🚨 chore: lint

--- src/lib/services/funds.ts
+++ src/lib/services/funds.ts
@@ -119,7 +119,7 @@ export class FundsService {
 			.getList<FundTransactionsResponse>(page, 10, {
 				filter: `created >= ${start} && created <= ${end} && description ~ "${query}" ${filterString}`,
 				expand: 'user',
-        sort: 'created'
+				sort: 'created'
 			});
 		const items = this.expandTranscations(transactionslist.items);
 
