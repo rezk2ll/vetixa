d0acc438ffae650c19fa3f22fd77a7fdac5f551e
Khaled FERJANI <kferjani@linagora.com>
2024-08-02 12:04:38 +0100

    ✨ feat: added cage color codes component

---
+++ src/lib/components/display/cages/CageColorCodes.svelte
@@ -0,0 +1,26 @@
+<div class="flex flex-col lg:flex-row w-full items-center justify-center">
+	<div class="w-full flex h-6 lg:h-12 items-center justify-center bg-red-500/80 text-white font-semibold">
+		Mort
+	</div>
+	<div
+		class="w-full flex h-6 lg:h-12 items-center justify-center bg-emerald-500/80 font-semibold text-gray-700"
+	>
+		Nouvelle admission
+	</div>
+	<div class="w-full flex h-6 lg:h-12 items-center justify-center bg-pink-500/80 text-white font-semibold">
+		Sortie
+	</div>
+	<div class="w-full flex h-6 lg:h-12 items-center justify-center bg-blue-500/80 text-white font-semibold">
+		Prolongation
+	</div>
+	<div
+		class="w-full flex h-6 lg:h-12 items-center justify-center bg-orange-500/80 text-white font-semibold"
+	>
+		À faire
+	</div>
+	<div
+		class="w-full flex h-6 lg:h-12 items-center justify-center bg-yellow-500 font-semibold text-slate-800"
+	>
+		Passation importante
+	</div>
+</div>
