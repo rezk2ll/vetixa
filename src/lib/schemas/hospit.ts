7d1e436f2dd9857c7d26d4de0281a3acffc021b5
Khaled FERJANI <kferjani@linagora.com>
2024-08-02 14:06:15 +0100

    🎨 fix: allow empty colors

--- src/lib/schemas/hospit.ts
+++ src/lib/schemas/hospit.ts
@@ -2,5 +2,5 @@ import { z } from 'zod';
 
 export const changeHospitColorsSchema = z.object({
 	id: z.string().min(1),
-	color: z.string().min(1, { message: 'Veuillez choisir une couleur' })
+	color: z.string().min(0, { message: 'Veuillez choisir une couleur' })
 });
