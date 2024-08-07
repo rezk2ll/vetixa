0009a1c81b371ce99b6ac79121e74755a9b6c619
Khaled FERJANI <kferjani@linagora.com>
2024-08-07 23:37:17 +0100

    🔧 chore: added times new roman font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
-	darkMode: 'class',
-	theme: {
-		extend: {
-			colors: {
-				blueGray: {
-					50: 'rgba(248, 250, 252, 1)',
-					100: 'rgba(241, 245, 249, 1)',
-					200: 'rgba(226, 232, 240, 1)',
-					600: 'rgba(71, 85, 105, 1)',
-					700: 'rgba(51, 65, 85, 1)',
-					800: 'rgba(30, 41, 59, 1)'
-				}
-			},
-			screens: {
-				print: { raw: 'print' },
-				screen: { raw: 'screen' }
-			}
-		}
-	},
-	plugins: []
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        blueGray: {
+          50: 'rgba(248, 250, 252, 1)',
+          100: 'rgba(241, 245, 249, 1)',
+          200: 'rgba(226, 232, 240, 1)',
+          600: 'rgba(71, 85, 105, 1)',
+          700: 'rgba(51, 65, 85, 1)',
+          800: 'rgba(30, 41, 59, 1)'
+        }
+      },
+      screens: {
+        print: { raw: 'print' },
+        screen: { raw: 'screen' }
+      },
+      fontFamily: {
+        timesnewroman: ['Times New Roman', 'serif']
+      }
+    }
+  },
+  plugins: []
 };
