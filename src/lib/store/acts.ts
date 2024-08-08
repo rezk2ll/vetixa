74850dfd4551e0be19c33ec86d29256bbee38f76
Khaled FERJANI <kferjani@linagora.com>
2024-08-08 14:19:56 +0100

    ✨ feat: added toilettage store

--- src/lib/store/acts.ts
+++ src/lib/store/acts.ts
@@ -1,6 +1,10 @@
 import { writable } from 'svelte/store';
-import type { ClinicalExamsResponse, MedicalActsResponse, SurgicalActsResponse } from '$types';
+import {
+	type ToilettageResponse,
+	type MedicalActsResponse,
+	type SurgicalActsResponse
+} from '$types';
 
 export const medicalActs = writable<MedicalActsResponse[]>();
 export const surgicalActs = writable<SurgicalActsResponse[]>();
-export const clinicalExams = writable<ClinicalExamsResponse[]>();
+export const toilettageActs = writable<ToilettageResponse[]>();
