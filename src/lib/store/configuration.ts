8c134e08132f26cfebc22c2e0929deff2ed57139
Khaled FERJANI <kferjani@linagora.com>
2024-08-22 16:23:08 +0100

    🐛 fix: use default logo

--- src/lib/store/configuration.ts
+++ src/lib/store/configuration.ts
@@ -2,5 +2,5 @@ import { type ConfigurationResponse } from '$types';
 import { writable } from 'svelte/store';
 
 export const configuration = writable<Partial<ConfigurationResponse>>({
-	logo: '/logo.svg'
+	logo: '/logo.png'
 });
